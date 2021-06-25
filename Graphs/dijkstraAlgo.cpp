0f5bd5b6ce761ce8e0987ba66fca859d574e7113
vivek singh <vikky30singh@gmail.com>
2021-06-25 15:19:01 +0530

    dijkstra's Algorithm for finding shortest path

---
+++ Graphs/dijkstraAlgo.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define null NULL
+#define inf INT_MAX
+#define minf INT_MIN
+#define ll long long int
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<int, string> pis;
+typedef pair<string, int> psi;
+typedef pair<string, string> pss;
+typedef vector<pii> vii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef map<int, int> mii;
+typedef map<string, string> mss;
+typedef map<int, string> mis;
+typedef map<string, int> msi;
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i, s, n) for (ll i = s; i < n; i++)
+#define mp make_pair
+#define pb push_back
+#define ump unordered_map
+#define pq priority_queue
+#define fi first
+#define se second
+#define INF 2e18
+
+void buildGraph(map<int, list<pii > > &adj, int a, int b, int w)
+{
+  adj[a].push_back({b, w});
+  adj[b].push_back({a, w});
+}
+
+int* dijkstra(map<int, list<pii > > &adj, int src)
+{
+  int v = adj.size();
+  int *dist = new int[v];
+
+  f(i, src, v)
+    dist[i] = inf;
+
+  dist[src] = 0;
+
+  multiset<pii > s;
+  s.insert({dist[src], src}); // set keeps data sorted acc. to first arg. so we have used distance as 1st arg.
+  
+  while (!s.empty())
+  {
+    auto it = *(s.begin());
+    s.erase(s.begin());
+
+    for (auto v : adj[it.se])
+    {
+      if ((dist[v.fi] > it.fi + v.se) and (it.fi != inf))
+      {
+        dist[v.fi] = it.fi + v.se;
+        s.insert({dist[v.fi], v.fi});
+      }
+    }
+  }
+
+  return dist;
+}
+
+int main()
+{
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int v, e, w, a, b;
+  map<int, list<pii > > adj;
+
+  cout << "Enter total no of vertex and edges\n";
+  cin >> v >> e;
+
+  cout << "Enter edges with weight\n";
+  f(i, 0, e)
+  {
+    cin >> a >> b >> w;
+    buildGraph(adj, a, b, w);
+  }
+
+  int src = 0;
+  int *dist = dijkstra(adj, src);
+
+  cout << "Shortest path is\n";
+  f(i, src, adj.size())
+    cout << src << " -> " << i << " (" << dist[i] << ")\n";
+
+  delete[] dist;
+  dist = NULL;
+
+  return 0;
+}
