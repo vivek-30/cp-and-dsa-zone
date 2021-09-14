406186c486cb5a344f5b603e9ffd5f4ff22308cf
vivek singh <vikky30singh@gmail.com>
2021-09-14 14:33:03 +0530

    sept2021 global codeforces

---
+++ sept2021Global/seatingArrangements.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define null NULL
+#define endl "\n"
+#define inf INT_MAX
+#define minf INT_MIN
+#define mod 1000000007
+#define ll long long int
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<int,string> pis;
+typedef pair<string,int> psi;
+typedef pair<string,string> pss;
+typedef vector<pii > vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<vi > vvi;
+typedef vector<vll > vvll;
+typedef map<int, int> mii;
+typedef map<string, string> mss;
+typedef map<int, string> mis;
+typedef map<string, int> msi;
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i,s,n) for(ll i = s; i <= n; i++)
+#define fr(i,s,e) for(ll i = s; i >= e; i--)
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
+#define bitc(n) __builtin_popcount(n)
+#define mp make_pair
+#define pb push_back
+#define ump unordered_map
+#define pqmax priority_queue<int, vi>
+#define pqmin priority_queue<int, vi, greater<int> >
+#define fi first
+#define se second
+#define INF 2e18
+
+vi arr;
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  
+  clock_t begin = clock();
+
+  ll t;
+  cin>>t;
+  f(i, 1, t) {
+    int n;
+    cin>>n;
+    arr.assign(n, -1);
+    f(j, 0, n-1) cin>>arr[j];
+    logarr(arr, 0, n-1);
+  }
+
+  #ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+  #endif
+
+  return 0;
+}
