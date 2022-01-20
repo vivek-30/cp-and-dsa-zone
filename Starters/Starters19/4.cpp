48c35b6e7cff4ce67e19f19268bd2403943fbb95
vivek singh <vikky30singh@gmail.com>
2022-01-20 12:45:27 +0530

    Starters 19

---
+++ Starters/Starters19/4.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define null NULL
+#define endl "\n"
+#define inf INT_MAX
+#define minf INT_MIN
+#define mod 1000000007
+#define ll long long int
+#define all(v) v.begin(), v.end()
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii > vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vi > vvi;
+typedef vector<vll > vvll;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+template<typename... T>
+void inp(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void out(T&&... args) { ((cout << args), ...);}
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i, s, e) for(ll i = s; i <= e; i++)
+#define fr(i, s, e) for(ll i = s; i >= e; i--)
+#define logarr(s, e, arr) for(int i = s; i <= e; i++) cout<<arr[i]<<" "; cout<<endl;
+#define bitc(n) __builtin_popcount(n)
+#define mp make_pair
+#define pb push_back
+#define sz size()
+#define ump unordered_map
+#define pqmax priority_queue<int, vi>
+#define pqmin priority_queue<int, vi, greater<int> >
+#define py cout<<"YES"<<endl;
+#define pn cout<<"NO"<<endl;
+#define fi first
+#define se second
+#define INF 2e18
+
+void solve() {
+  int n;
+  inp(n);
+
+  ll ans = 0;
+
+  vi arr(n);
+  f(i, 0, n-1) inp(arr[i]);
+
+  vi distinct(n);
+  set<int> s;
+
+  distinct[n-1] = 0;
+
+  fr(i, n-2, 0) {
+    s.insert(arr[i+1]);
+    distinct[i] = s.sz;
+  }
+
+  int j;
+  f(i, 0, n-1) {
+    j = s.find(arr[i]) != s.end() ? distinct[i]-1 : distinct[i];
+    if(j < 0) j = 0;
+    ans = (ans % mod + (1<<j) % mod) % mod;
+  }
+
+  out(ans, '\n');
+
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  clock_t begin = clock();
+
+  int t;
+  cin>>t;
+
+  while(t--) {
+    solve();
+  }
+
+  #ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+  #endif
+
+  return 0;
+}
