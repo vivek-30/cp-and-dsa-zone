a47f426efbb1e5cf4612d4750d4f5f3021d4d19b
vivek singh <vikky30singh@gmail.com>
2022-04-27 22:20:18 +0530

    Educational Contest April2022

---
+++ EducationalContests/April2022/c.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inf INT_MAX
+#define minf INT_MIN
+#define mod 1000000007
+#define ll long long int
+#define ld long double
+#define all(v) v.begin(), v.end()
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii > vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vi > vvi;
+typedef vector<vll > vvll;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+template<typename... T>
+void inp(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void out(T&&... args) { ((cout << args), ...);}
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i, s, e) for(int i = s; i <= e; i++)
+#define fr(i, s, e) for(int i = s; i >= e; i--)
+#define logarr(s, e, arr) for(int i = s; i <= e; i++) cout << arr[i] << " ";  cout << "\n";
+#define bitc(n) __builtin_popcountll(n)
+#define debug(x) cout << #x << " = " << x << "\n";
+#define mp make_pair
+#define eb emplace_back
+#define sz size()
+#define br "\n"
+#define ump unordered_map
+#define pqmax priority_queue<int, vi>
+#define pqmin priority_queue<int, vi, greater<int> >
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define fi first
+#define se second
+#define INF 2e18
+
+void solve(int tc) {
+  int n, x;
+  inp(n, x);
+
+  vi arr(n);
+  f(i, 0, n-1) inp(arr[i]);
+
+  sort(all(arr));
+
+  vll prefix(n);
+  prefix[0] = arr[0];
+  f(i, 1, n-1) prefix[i] = prefix[i-1] + arr[i];
+
+  ll ans = 0, cnt = 0;
+  int s = 0, e = x, m, res = -1;
+  while(s <= e) {
+    m = mid(s, e);
+    if((prefix[n-1] + (n*m)) <= x) {
+      res = m;
+      s = m+1;
+    } else e = m-1;
+  }
+
+  if(res != -1) {
+    ans += (n * (res+1));
+    cnt = res + 1;
+  }
+
+  while(arr[0] + cnt <= x) {
+    int s = 0, e = n-1, m, res = 0;
+    while(s <= e) {
+      m = mid(s, e);
+      if((prefix[m] + ((m+1)*cnt)) <= x) {
+        res = m;
+        s = m+1;
+      } else e = m-1;
+    }
+
+    ans += (res+1);
+    cnt++;
+  }
+
+  out(ans, br);
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  clock_t begin = clock();
+
+  int tc = 1;
+  cin >> tc;
+
+  f(t, 1, tc) {
+    solve(t);
+  }
+
+  #ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+  #endif
+
+  return 0;
+}
