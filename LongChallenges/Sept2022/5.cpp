a321520796817facf8fe4c7b9359fe65c1594da3
vivek singh <vikky30singh@gmail.com>
2022-09-12 22:23:19 +0530

    Sept2022 Long Challenge

---
+++ LongChallenges/Sept2022/5.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define inf 2e18
+#define minf -2e18
+#define yes cout << "YES" << "\n";
+#define no cout << "NO" << "\n";
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define mid(s, e) (s + (e - s) / 2)
+#define all(v) v.begin(), v.end()
+#define rep(i, s, e) for(int i = s; i <= e; i++)
+#define reprev(i, s, e) for(int i = s; i >= e; i--)
+template<typename... T>
+void input(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void print(T&&... args) { ((cout << args), ...);}
+#define debug(x) cout << #x << " = " << x << '\n';
+#define bitcount(x) __builtin_popcountll(x)
+#define sz size()
+
+void solve() {
+  int n;
+  input(n);
+
+  vii arr(n);
+  rep(i, 0, n-1) input(arr[i].fi);
+  rep(i, 0, n-1) input(arr[i].se);
+
+  sort(all(arr));
+
+  int ans = 1, mx = arr[n-1].se;
+  reprev(i, n-2, 0) {
+    if(arr[i].se >= mx) ans++;
+    mx = max(mx, arr[i].se);
+  }
+
+  print(ans, '\n');
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while(t--) {
+    solve();
+  }
+
+  return 0;
+}
