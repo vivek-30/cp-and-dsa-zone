396c4398e04e0257a4cadc4cf582a6c3a4aae16f
vivek singh <vikky30singh@gmail.com>
2022-10-01 22:23:39 +0530

    Vivek Gupta DP WorkShop: Day2

---
+++ DP_WorkShop_Vivek_Gupta/Day2/2.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inf INT_MAX
+#define minf INT_MIN
+#define mod 1000000007
+#define ll long long int
+#define ld long double
+#define all(v) v.begin(), v.end()
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii > vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vi > vvi;
+typedef vector<vll > vvll;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i, s, e) for(int i = s; i <= e; i++)
+#define fr(i, s, e) for(int i = s; i >= e; i--)
+#define logarr(s, e, arr) for(int i = s; i <= e; i++) cout << arr[i] << " ";  cout << "\n";
+#define bitc(n) __builtin_popcountll(n)
+#define debug(x) cout << #x << " = " << x << "\n";
+#define mp make_pair
+#define eb emplace_back
+#define sz size()
+#define br "\n"
+#define ump unordered_map
+#define pqmax priority_queue<int, vi>
+#define pqmin priority_queue<int, vi, greater<int> >
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define fi first
+#define se second
+#define INF 2e18
+
+int findCost(int n, int w, vii &arr) {
+  if(n < 0) return 0;
+
+  int mx1 = findCost(n-1, w, arr);
+  int mx2 = 0;
+
+  if(arr[n].fi <= w) mx2 = arr[n].se + findCost(n-1, w-arr[n].fi, arr);
+
+  return max(mx1, mx2);
+}
+
+void solve(int tc) {
+  int n, w;
+  cin>>n>>w;
+
+  vii arr(n);
+  f(i, 0, n-1) cin>>arr[i].fi>>arr[i].se;
+
+  cout<<findCost(n-1, w, arr)<<endl;
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  clock_t begin = clock();
+
+  int tc = 1;
+  cin >> tc;
+
+  f(t, 1, tc) {
+    solve(t);
+  }
+
+  #ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+  #endif
+
+  return 0;
+}
