7a427b9038b2e3c835d6a6ee4bc5ca94a16fbf4e
vivek singh <vikky30singh@gmail.com>
2022-10-01 22:25:13 +0530

    Vivek Gupta DP WorkShop: Day3

---
+++ DP_WorkShop_Vivek_Gupta/Day3/1.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inf INT_MAX
+#define minf INT_MIN
+#define mod 1000000007
+#define ll long long int
+#define ld long double
+#define all(v) v.begin(), v.end()
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii > vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vi > vvi;
+typedef vector<vll > vvll;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i, s, e) for(int i = s; i <= e; i++)
+#define fr(i, s, e) for(int i = s; i >= e; i--)
+#define logarr(s, e, arr) for(int i = s; i <= e; i++) cout << arr[i] << " ";  cout << "\n";
+#define bitc(n) __builtin_popcountll(n)
+#define debug(x) cout << #x << " = " << x << "\n";
+#define mp make_pair
+#define eb emplace_back
+#define sz size()
+#define br "\n"
+#define ump unordered_map
+#define pqmax priority_queue<int, vi>
+#define pqmin priority_queue<int, vi, greater<int> >
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define fi first
+#define se second
+#define INF 2e18
+
+ll arr[1005][1005], dp[1005][1005];
+
+ll getAns(int n, int m) {
+  if(n < 0 or m < 0) return minf;
+
+  if(n == 0 and m == 0) return arr[n][m];
+
+  if(dp[n][m] != -1) return dp[n][m];
+
+  ll ans = 0;
+  ll mx1 = getAns(n-1, m);
+  ll mx2 = getAns(n, m-1);
+
+  if(mx1 != minf) ans = max(ans, mx1+arr[n][m]);
+  if(mx2 != minf) ans = max(ans, mx2+arr[n][m]);
+  return dp[n][m] = max(ans, arr[n][m]);
+}
+
+void solve(int tc) {
+  int n, m;
+  cin >> n >> m;
+
+  f(i, 0, n-1)
+    f(j, 0, m-1) cin>>arr[i][j];
+
+  memset(dp, -1, sizeof(dp));
+
+  cout<<getAns(n-1, m-1)<<endl;
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  clock_t begin = clock();
+
+  int tc = 1;
+  cin >> tc;
+
+  f(t, 1, tc) {
+    solve(t);
+  }
+
+  #ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+  #endif
+
+  return 0;
+}
---
+++ DP_WorkShop_Vivek_Gupta/Day3/3.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inf INT_MAX
+#define minf INT_MIN
+#define mod 1000000007
+#define ll long long int
+#define ld long double
+#define all(v) v.begin(), v.end()
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii > vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vi > vvi;
+typedef vector<vll > vvll;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+#define mid(l, r) (l + (r - l) / 2)
+#define f(i, s, e) for(int i = s; i <= e; i++)
+#define fr(i, s, e) for(int i = s; i >= e; i--)
+#define logarr(s, e, arr) for(int i = s; i <= e; i++) cout << arr[i] << " ";  cout << "\n";
+#define bitc(n) __builtin_popcountll(n)
+#define debug(x) cout << #x << " = " << x << "\n";
+#define mp make_pair
+#define eb emplace_back
+#define sz size()
+#define br "\n"
+#define ump unordered_map
+#define pqmax priority_queue<int, vi>
+#define pqmin priority_queue<int, vi, greater<int> >
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define fi first
+#define se second
+#define INF 2e18
+
+int arr[1005][1005];
+
+void solve(int tc) {
+  int n, m;
+  cin>>n>>m;
+
+  f(i, 1, n) f(j, 1, m) cin>>arr[i][j];
+
+  vvll dp(n+1, vll(m+1, 0));
+  ll ans = minf;
+  f(i, 1, n) {
+    f(j, 1, m) {
+      if(arr[i][j]) {
+        dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
+      }
+      ans = max(ans, dp[i][j]);
+    }
+  }
+  
+  cout<<ans*ans<<endl;
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  clock_t begin = clock();
+
+  int tc = 1;
+  cin >> tc;
+
+  f(t, 1, tc) {
+    solve(t);
+  }
+
+  #ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+  #endif
+
+  return 0;
+}
+
