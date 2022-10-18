58998e213b6d2e85ce2dcb3a644781c6e73ccac9
vivek singh <vikky30singh@gmail.com>
2022-10-18 18:51:57 +0530

    Random Problems

---
+++ Random/buildingRace.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define inf 2e18
+#define minf -2e18
+#define yes cout << "YES" << "\n";
+#define no cout << "NO" << "\n";
+#define pii pair<ll, ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define mid(s, e) (s + (e - s) / 2)
+#define all(v) v.begin(), v.end()
+#define rep(i, s, e) for(int i = s; i <= e; i++)
+#define reprev(i, s, e) for(int i = s; i >= e; i--)
+template<typename... T>
+void input(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void output(T&&... args) { ((cout << args), ...);}
+#define debug(x) cout << #x << " = " << x << '\n';
+#define bitcount(x) __builtin_popcountll(x)
+#define sz size()
+
+void solve() {
+  double a, b, x, y;
+  input(a, b, x, y);
+  
+  double val1 = a/x, val2 = b/y;
+  if(val1 == val2) {
+    output("both\n");
+  } else if(val1 > val2) output("chefina\n");
+  else output("chef\n");
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while(t--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ Random/singleOperationPart1.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define inf 2e18
+#define minf -2e18
+#define yes cout << "YES" << "\n";
+#define no cout << "NO" << "\n";
+#define pii pair<ll, ll>
+#define vi vector<int>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define mid(s, e) (s + (e - s) / 2)
+#define all(v) v.begin(), v.end()
+#define rep(i, s, e) for(int i = s; i <= e; i++)
+#define reprev(i, s, e) for(int i = s; i >= e; i--)
+template<typename... T>
+void input(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void output(T&&... args) { ((cout << args), ...);}
+#define debug(x) cout << #x << " = " << x << '\n';
+#define bitcount(x) __builtin_popcountll(x)
+#define sz size()
+
+void solve() {
+  int n;
+  string s;
+
+  input(n, s);
+
+  int ans = 0;
+  reverse(all(s));
+
+  rep(i, 0, n-1) {
+    if(s[i] == '0') {
+      ans = i+1;
+    }
+  }
+
+  output(n - ans, '\n');
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while(t--) {
+    solve();
+  }
+
+  return 0;
+}
